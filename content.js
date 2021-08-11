76c44ba60636b6abbfc5d9c1b126a071551284e9
RK <krishna1492000@gmail.com>
2021-08-11 13:30:47 +0530

    modified to get response from api using fetch instead of XMLlHttpRequest

--- content.js
+++ content.js
@@ -1,16 +1,30 @@
 // Add functionality to highlight important parts of text
-// use fetch instead of xml http request
 // add code to handle incorrect input cases
 
-let xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.addEventListener("readystatechange", function () {
-	if (this.readyState === this.DONE) {
-		console.log(this.responseText);
-        let index=this.responseText.search("article_text");
-        let summary = this.responseText.substring(13,index-4);
-        console.log('Response : ',summary);
+function summarize(url){
+    var response;
+    const data = JSON.stringify({
+        "url": url,
+        "min_length": 100,
+        "max_length": 300,
+        "is_detailed": false
+    });
+    fetch("https://tldrthis.p.rapidapi.com/v1/model/abstractive/summarize-url/", {
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "x-rapidapi-key": "token",
+            "x-rapidapi-host": "tldrthis.p.rapidapi.com"
+        },
+	    "body": data
+    })
+    .then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        // `data` is the parsed version of the JSON returned from the above endpoint.
+        console.log(data);
+        response = data.summary[0];
+        console.log('Response : ',response);
         const content = `
         <div class="eve-summary">
             <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -19,7 +33,7 @@ xhr.addEventListener("readystatechange", function () {
                     <div class="w3-container">
                         <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-display-topright">&times;</span>
                         <h1> Text Summary </h1>
-                        <p style="margin-bottom: 2em">${summary}</p>
+                        <p style="margin-bottom: 2em">${response}</p>
                     </div>
                 </div>
             </div>
@@ -29,23 +43,7 @@ xhr.addEventListener("readystatechange", function () {
         newDiv.innerHTML = content;
         document.querySelector("body").prepend(newDiv);
         document.getElementById('id01').style.display='block';
-    }
-});
-
-xhr.open("POST", "https://tldrthis.p.rapidapi.com/v1/model/abstractive/summarize-url/");
-xhr.setRequestHeader("content-type", "application/json");
-xhr.setRequestHeader("x-rapidapi-key", "token");
-xhr.setRequestHeader("x-rapidapi-host", "tldrthis.p.rapidapi.com");
-
-
-function summarize(url){
-    const data = JSON.stringify({
-        "url": url,
-        "min_length": 100,
-        "max_length": 300,
-        "is_detailed": false
     });
-    xhr.send(data); 
 }
 
 function highlight() {
